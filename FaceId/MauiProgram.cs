dc3f293eb08291d850a536adc46695cd9b9315ab
Steve Davis <sdavis@lcisystems.com>
2025-08-08 15:19:23 +0500

    _BioMetric working

--- FaceId/MauiProgram.cs
+++ FaceId/MauiProgram.cs
@@ -1,27 +1,28 @@
 ﻿using Microsoft.Extensions.Logging;
+using Plugin.Maui.Biometric;
 
-namespace FaceId
+namespace FaceId;
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+           // .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
         {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+        });
+        builder.Services.AddMauiBlazorWebView();
 
-            builder.Services.AddMauiBlazorWebView();
+        // Use with Dependency Injection
+        builder.Services.AddSingleton<IBiometric>(BiometricAuthenticationService.Default);
 
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
-
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
